e80f6222c061abeb603c1c9772df1119c0cbcb24
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2020-11-27 05:06:09 -0300

    First commit

---
+++ lib/FinalHomePage.dart
@@ -0,0 +1,280 @@
+import 'package:flutter/material.dart';
+import 'StopwatchPage.dart';
+
+class DefineTimesPage extends StatefulWidget {
+  final Color color;
+
+  DefineTimesPage({this.color});
+
+  @override
+  DefineTimesPageState createState() => DefineTimesPageState();
+}
+
+class DefineTimesPageState extends State<DefineTimesPage> {
+  TextEditingController controladorAtivo = TextEditingController();
+  TextEditingController controladorDescanso = TextEditingController();
+  TextEditingController controladorQntdSeries = TextEditingController();
+  TextEditingController controladorQntdRepeticao = TextEditingController();
+
+  bool flagContinuar = true;
+  bool flagStart = true;
+  bool flagTempos = true;
+
+  int qntdConfiguracoes = 0;
+
+  List<Map<String, dynamic>> listaTempos = [];
+
+  void addSerie() {
+    listaTempos.add({
+      "tempoDescanso": int.parse(controladorDescanso.text),
+      "tempoAtivo": int.parse(controladorAtivo.text),
+      "controladorQntdRepeticao": int.parse(controladorQntdRepeticao.text),
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Jump Rope",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: SafeArea(
+          child: Container(
+            child: Center(
+              child: flagContinuar
+                  ? ButtonTheme(
+                      minWidth: 250.0,
+                      height: 100.0,
+                      shape: const StadiumBorder(),
+                      child: RaisedButton(
+                        color: Colors.green[700],
+                        child: Text(
+                          "Começar",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            flagContinuar = false;
+                          });
+                        },
+                      ),
+                    )
+                  : SingleChildScrollView(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Column(
+                            children: [
+                              flagTempos
+                                  ? Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: TextFormField(
+                                        controller: controladorQntdSeries,
+                                        keyboardType: TextInputType.number,
+                                        textAlign: TextAlign.center,
+                                        decoration: InputDecoration(
+                                          labelText: "Quantas séries",
+                                          labelStyle: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    )
+                                  : Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        "$qntdConfiguracoes SÉRIES",
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                              (flagTempos || !flagStart)
+                                  ? Container()
+                                  : Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: TextFormField(
+                                        controller: controladorDescanso,
+                                        keyboardType: TextInputType.number,
+                                        textAlign: TextAlign.center,
+                                        decoration: InputDecoration(
+                                          labelText: "Quanto tempo de descanso",
+                                          labelStyle: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                              (flagTempos || !flagStart)
+                                  ? Container()
+                                  : Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: TextFormField(
+                                        controller: controladorAtivo,
+                                        keyboardType: TextInputType.number,
+                                        textAlign: TextAlign.center,
+                                        decoration: InputDecoration(
+                                          labelText: "Quanto tempo ativo",
+                                          labelStyle:
+                                              TextStyle(color: Colors.white),
+                                        ),
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                              (flagTempos || !flagStart)
+                                  ? Container()
+                                  : Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: TextFormField(
+                                        controller: controladorQntdRepeticao,
+                                        keyboardType: TextInputType.number,
+                                        textAlign: TextAlign.center,
+                                        decoration: InputDecoration(
+                                          labelText: "Quantas repetições",
+                                          labelStyle:
+                                              TextStyle(color: Colors.white),
+                                        ),
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 100),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                RaisedButton(
+                                  padding: const EdgeInsets.all(4),
+                                  color: Colors.lightBlue,
+                                  shape: const StadiumBorder(),
+                                  child: const Text(
+                                    'Voltar',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      flagContinuar = true;
+                                      flagTempos = true;
+                                      controladorQntdSeries.text = "";
+                                      controladorDescanso.text = "";
+                                      controladorAtivo.text = "";
+                                      controladorQntdRepeticao.text = "";
+                                    });
+                                  },
+                                ),
+                                flagStart
+                                    ? Container()
+                                    : ButtonTheme(
+                                        minWidth: 125,
+                                        height: 50,
+                                        shape: const StadiumBorder(),
+                                        child: RaisedButton(
+                                          color: Colors.green[700],
+                                          child: Text(
+                                            "Começar",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 25,
+                                            ),
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              controladorDescanso.text = "";
+                                              controladorAtivo.text = "";
+                                              controladorQntdRepeticao.text =
+                                                  "";
+
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      StopwatchPage(
+                                                    vetorAtivo: listaTempos,
+                                                  ),
+                                                ),
+                                              );
+                                            });
+                                          },
+                                        ),
+                                      ),
+                                RaisedButton(
+                                  padding: const EdgeInsets.all(4),
+                                  color: Colors.lightBlue,
+                                  shape: const StadiumBorder(),
+                                  child: Text(
+                                    flagTempos ? "Iniciar" : "Adicionar",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      if (controladorQntdSeries.text != "") {
+                                        qntdConfiguracoes = int.parse(
+                                            controladorQntdSeries.text);
+                                        flagTempos = false;
+                                      }
+
+                                      if (controladorDescanso.text != "" &&
+                                          controladorAtivo.text != "" &&
+                                          controladorQntdRepeticao.text != "") {
+                                        addSerie();
+
+                                        if (qntdConfiguracoes ==
+                                            listaTempos.length) {
+                                          flagStart = false;
+                                        }
+
+                                        controladorDescanso.text = "";
+                                        controladorAtivo.text = "";
+                                        controladorQntdRepeticao.text = "";
+                                      }
+                                    });
+                                  },
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/StopwatchPage.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
+
+class StopwatchPage extends StatefulWidget {
+  List<Map<String, dynamic>> vetorAtivo;
+
+  StopwatchPage({this.vetorAtivo});
+
+  @override
+  _StopwatchPageState createState() => _StopwatchPageState();
+}
+
+class _StopwatchPageState extends State<StopwatchPage> {
+  int _ativo = 0;
+  int _descanso = 0;
+  int _count = 0;
+  int _serieAtual = 0;
+
+  bool _flagMudarFases = false;
+  bool _flagMudarAtivoDescanso = false;
+  bool _isPaused = false;
+
+  ColorSwatch _color = Colors.blue;
+
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer();
+
+  @override
+  void initState() {
+    super.initState();
+    _stopWatchTimer.secondTime.listen((value) {
+      if (value >= (_flagMudarAtivoDescanso ? _ativo : _descanso) + 1) {
+        setState(() {
+          _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+          _color = (_flagMudarAtivoDescanso ? Colors.blue : Colors.red);
+          _stopWatchTimer.onExecute.add(StopWatchExecute.reset);
+          _stopWatchTimer.onExecute.add(StopWatchExecute.start);
+          _count--;
+          _flagMudarAtivoDescanso = !_flagMudarAtivoDescanso;
+
+          if (_count == 0) _flagMudarFases = true;
+
+          if (_flagMudarFases) {
+            int lenght = widget.vetorAtivo.length;
+
+            if (_serieAtual == lenght)
+              _serieAtual = 0;
+            else
+              _serieAtual++;
+
+            _ativo = widget.vetorAtivo[_serieAtual]["tempoAtivo"];
+            _descanso = widget.vetorAtivo[_serieAtual]["tempoDescanso"];
+            _count =
+                widget.vetorAtivo[_serieAtual]["controladorQntdRepeticao"] * 2;
+
+            _flagMudarFases = false;
+          }
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await _stopWatchTimer.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: _color,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Jump Rope",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        color: _color,
+        child: SafeArea(
+          child: Container(
+            color: _color,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: StreamBuilder<int>(
+                        stream: _stopWatchTimer.secondTime,
+                        initialData: _stopWatchTimer.secondTime.value,
+                        builder: (context, snap) {
+                          final value = snap.data;
+                          return Container(
+                            child: Text(
+                              value.toString(),
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 80,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ButtonTheme(
+                          minWidth: 100,
+                          height: 60,
+                          shape: const StadiumBorder(),
+                          child: RaisedButton(
+                            padding: const EdgeInsets.all(4),
+                            color: Colors.green,
+                            shape: const StadiumBorder(),
+                            child: Text(
+                              _isPaused ? "Voltar" : 'Começar',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () async {
+                              _stopWatchTimer.onExecute
+                                  .add(StopWatchExecute.start);
+                              _ativo =
+                                  widget.vetorAtivo[_serieAtual]["tempoAtivo"];
+                              _descanso = widget.vetorAtivo[_serieAtual]
+                                  ["tempoDescanso"];
+                              _count = widget.vetorAtivo[_serieAtual]
+                                      ["controladorQntdRepeticao"] *
+                                  2;
+
+                              setState(() {
+                                _isPaused = false;
+                              });
+                            },
+                          ),
+                        ),
+                        RaisedButton(
+                          padding: const EdgeInsets.all(4),
+                          color: Colors.lightBlue,
+                          shape: const StadiumBorder(),
+                          onPressed: () async {
+                            setState(() {
+                              _ativo =
+                                  widget.vetorAtivo[_serieAtual]["tempoAtivo"];
+                              _descanso = widget.vetorAtivo[_serieAtual]
+                                  ["tempoDescanso"];
+                              _count = widget.vetorAtivo[_serieAtual]
+                                      ["controladorQntdRepeticao"] *
+                                  2;
+
+                              _flagMudarFases = false;
+                              _flagMudarAtivoDescanso = false;
+                              _color = Colors.blue;
+                            });
+                            _stopWatchTimer.onExecute
+                                .add(StopWatchExecute.stop);
+                            _stopWatchTimer.onExecute
+                                .add(StopWatchExecute.reset);
+                          },
+                          child: const Text(
+                            'Reset',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        ButtonTheme(
+                          minWidth: 100,
+                          height: 60,
+                          shape: const StadiumBorder(),
+                          child: RaisedButton(
+                            padding: const EdgeInsets.all(4),
+                            color: Colors.deepPurpleAccent,
+                            shape: const StadiumBorder(),
+                            onPressed: () async {
+                              _stopWatchTimer.onExecute
+                                  .add(StopWatchExecute.stop);
+                              setState(() {
+                                _isPaused = true;
+                              });
+                            },
+                            child: const Text(
+                              "Pausar",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
